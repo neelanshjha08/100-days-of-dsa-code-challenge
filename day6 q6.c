be280430d3e2891c6035c17878fbb3ca6cde4516
neelanshjha08 <neelanshjha15@gmail.com>
2026-02-07 22:58:37 +0530

    Add files via upload

---
+++ day6 q6.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Print first element (always unique)
+    printf("%d ", arr[0]);
+
+    // Check remaining elements
+    for (int i = 1; i < n; i++) {
+        if (arr[i] != arr[i - 1]) {
+            printf("%d ", arr[i]);
+        }
+    }
+
+    return 0;
+}
